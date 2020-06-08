64f2da75ca07bd897b7862f52a69f24f51fbf0fb
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-08 15:36:54 -0700

    adjust scripts so that form is cleared each time and then re-displayed as new list when new items added
    Co-Authored-By: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-$(document).ready(function()  {
-  $("input.")
+$(document).ready(function(){
+  const itemsArray = [];
+  $("form").submit(function(event)  {
+    event.preventDefault();
+    itemsArray.push($("#item").val());
+    console.log(itemsArray);
+  });
+  $("button#showList").click(function()  {
+    $("ol").empty();
+    itemsArray.sort();
+    itemsArray.forEach(function(item) {
+      $("ol").append("<li>" + item.toUpperCase() + "</li>");
+    })
+  })
 })
